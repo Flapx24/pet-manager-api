f0eef4edc540f997cf2b3ae6f0bc4a560aec5732
Flapx24 <bares.gafer23@cadiz.salesianos.edu>
2025-05-07 09:49:03 +0200

    First commit

---
+++ src/main/java/com/example/demo/PetManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetManagerApiApplication.class, args);
+	}
+
+}
